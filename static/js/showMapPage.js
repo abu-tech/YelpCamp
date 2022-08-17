1b1e6d369653c58d420bc985c9ba891120e83027
Mohammed samir <abutech8801@gmail.com>
2022-08-17 17:35:25 +0530

    Update Show map page

--- static/js/showMapPage.js
+++ static/js/showMapPage.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: "mapbox://styles/mapbox/light-v10", // style URL
     center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 10, // starting zoom
+    zoom: 7, // starting zoom
     projection: 'globe' // display the map as a 3D globe
 });
 
